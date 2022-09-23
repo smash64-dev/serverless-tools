df9f2d3287e6dd0b321560461640e3710dbaff59
CEnnis91 <cennis91@gmail.com>
2022-09-23 14:45:41 -0400

    Add ability to set host to 'self' Change 'from' to 'via'

--- p2p-check/src/app.py
+++ p2p-check/src/app.py
@@ -7,13 +7,14 @@ import kaillera.p2pclient as Kaillera
 
 
 # FIXME: the number of messages is limited until PING is implemented
-def bot_message_en(user: str, host: str, port: int, check_from: str):
+def bot_message_en(user: str, host: str, port: int,
+                   check_from: str, check_via: str):
     return [
         f'\n',
         f"Hey {user}! Your P2P is WORKING CORRECTLY!",
         f"Just share '{host}:{port}' with your opponent to play.\n",
         f'Remember to only share your IP with people you trust.',
-        f'This bot check came from {check_from}.',
+        f'This bot request came from {check_from} via {check_via}.',
         f'If you did not ask for this check, please report it in Discord.\n',
         f'ggs',
     ]
@@ -37,9 +38,12 @@ def respond(http_code: int, success: bool, message: str):
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
-        host = body['host']
+        header = 'cf-connecting-ip'
+
         port = int(body['port']) if 'port' in body else 27886
-        check_from = body['from'] if 'from' in body else 'unknown'
+        check_via = body['via'] if 'via' in body else 'unknown'
+        check_from = event.get('headers', {}).get(header, 'unknown')
+        host = check_from if body['host'] == 'self' else body['host']
 
         client = Kaillera.P2PClient(host, port)
     except socket.error:
@@ -54,15 +58,17 @@ def lambda_handler(event, context):
 
         if connect is True:
             user, game = client.get_host_info(response)
-            message = bot_message_en(user, host, port, check_from)
+            message = bot_message_en(user, host, port, check_from, check_via)
 
             for id, line in enumerate(message):
                 wait = True if id == 0 else False
                 client.chat(line, wait=wait)
             client.disconnect()
 
-            #return respond(200, True, json.dumps({'user': user, 'game': game}))
-            return respond(200, True, json.dumps(event))
+            return respond(200, True, json.dumps({
+                'host': host, 'port': port,
+                'user': user, 'game': game,
+            }))
         else:
             return respond(200, False, "Unable to connect")
     except Exception as e:
